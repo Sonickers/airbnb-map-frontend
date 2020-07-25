233454462b4916ddd4a37ae07229ebdee6e15be9
Sonickers <soniarepec@gmail.com>
2020-07-25 18:57:42 +0200

    Add google-map library and delete useless files

---
+++ src/components/Map.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { withGoogleMap, GoogleMap } from 'react-google-maps'
+import { PlaceMarker } from './PlaceMarker'
+
+const AirbnbMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultCenter={props.center}
+    defaultZoom={props.zoom}>
+    {props.places}
+    </GoogleMap>
+));
+
+export class Map extends Component {
+  constructor(props) {
+    super(props)
+
+    this.zoom = 7
+
+    this.state = {
+      lat: 50.0515918,
+      lng: 19.9357531
+    };
+  }
+
+  render() {
+    const {lat, lng} = this.state;
+    const places = [<PlaceMarker key={1} lat={lat} lng={lng} price={20} />]
+
+    return(
+      <div style={{width: `750px`, height: `750px`}}>
+        <AirbnbMap
+          center={{
+            lat: lat,
+            lng: lng
+          }}
+          places={places}
+          zoom={this.zoom}
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+        />
+      </div>
+    );
+  }
+}
